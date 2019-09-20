d9e39dac160f1f4ba01319e75f914f2ffe909c93
mikaparn <mikaparn@LSE-B3984>
2019-09-20 11:26:07 +0200

    Started If/Else statements

--- projects/Repetition/assets/js/test.js
+++ projects/Repetition/assets/js/test.js
@@ -1,1 +1,70 @@
-alert("are we conected?")
+// alert("Pop-up-ruta")
+
+var myName = "Mika"; //Datatyp: String
+var myAge = 18; //Datatyp: Number
+var isEvil = false; //Datatyp: Boolean
+
+// Single line comment
+/*
+    ************
+    Block comment
+    ************
+*/
+
+/*
+    ------------- 
+        if/else
+    -------------
+*/
+
+if(myAge < 18) {
+    //alert("You can not take a drivers license.")
+} else {
+    //alert("Congratz! You are old enough to drive.")
+}
+
+if(myName == "Mika") {
+    //alert("Welcome, " + myName);
+} else {
+    //alert("You are not welcome here!")
+}
+
+// == Equals to, === equal to and same datatype.
+if(myAge === "18") { 
+    //alert("Welcome");
+} else {
+    //alert("Not welcome");
+}
+
+var hasWatched = false; // add to watchlist
+
+if(isEvil == true) {
+    //alert("You are Evil!");
+} else {
+    //alert("You are Nice!");
+}
+
+/* 
+    *****************
+       Guessing app
+    *****************
+*/
+
+var secretNumber = 7;
+var userInput = Number(prompt("Guess a number between 0 - 10"));
+
+if(userInput === secretNumber) {
+    document.writeln("Congratulations! You guessed right : <br>");
+}
+else if(userInput === 6 || 8 ) {
+    document.writeln("Det bränns");
+
+}
+else if(userInput > secretNumber) {
+    document.writeln("Du gissade För högt");
+}
+else if(userInput < secretNumber) {
+    document.writeln("Du gissade för lågt");
+} else {
+    
+}
