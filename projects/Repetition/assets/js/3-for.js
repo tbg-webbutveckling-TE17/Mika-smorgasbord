acb28f86211450d709b3a5dbda53578404d5e166
mikaparn <mikaparn@LSE-B3984>
2019-10-02 12:42:59 +0200

    continued working on for

--- projects/Repetition/assets/js/3-for.js
+++ projects/Repetition/assets/js/3-for.js
@@ -1,20 +1,39 @@
 // for använder man när man vet hur många gånger loopen ska köra
 
-// initieering; villkor, inkrementering
-for(var i = 0; i <= 6; i++) {
+// initiering; villkor; inkrementering
+//for(var i = 0; i <= 6; i++) {
     // kör koden för varje iteration
+//    console.log(i);
+
+//}
+
+//var str = "Hello World!"
+//for(var i = 0; i < str.length; i++){
+//    console.log(str[i]);
+
+
+//}
+//var myArr = ["html", "css", "javascript", 42, true];
+//var len = myArr.length;
+//for (var i = 0; i < len; i++) {
+//    console.log(myArr[i]);
+//}
+
+for (var i = -10; i <= 19; i++) {
     console.log(i);
-
 }
-
-var str = "Hello World!"
-for(var i = 0; i < str.length; i++){
-    console.log(str[i]);
-
-
+for (var i = 10; i <= 40; i += 2) {
+    if(i % 2 === 0) {
+        console.log(i);
+    }
 }
-var myArr = ["html", "css", "javascript", 42, true];
-var len = myArr.length;
-for (var i = 0; i < len; i++) {
-    console.log(myArr[i]);
+for (var i = 301; i <= 333; i += 2) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+for (var i = 5; i <= 50; i ++) {
+    if (i % 3 == 0 && i % 5 == 0)  {
+    console.log(i);
+}
 }
