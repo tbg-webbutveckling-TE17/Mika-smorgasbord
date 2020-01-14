5e71c46a55a09c6d497e7c25025d64fdeaf08266
mikaparn <mikaparn@LSE-B3984>
2020-01-14 14:01:02 +0100

    continued with 5-array.js

--- projects/Repetition/assets/js/5-array.js
+++ projects/Repetition/assets/js/5-array.js
@@ -1,10 +1,10 @@
+/*
 var friend1 = "Måns";
 var friend2 = "Sten";
 var friend3 = "Claerence";
 var friend4 = "Mona";
 
 var friends = ["Måns", "Sten", "Claerence", "Mona"];
-var prinToScreen = document.getElementById("testArray");
 prinToScreen.innerHTML = friends[1];
 
 function newName() {
@@ -21,3 +21,36 @@ prinToScreen.innerHTML = colors.join(" <br /> ");
 
 colors.pop();
 prinToScreen.innerHTML = colors.join(" <br /> ");
+
+colors.push("purple");
+prinToScreen.innerHTML = colors.join(" <br /> ");
+
+colors.unshift("yellow");
+prinToScreen.innerHTML = colors.join(" <br /> ");
+
+colors.shift();
+prinToScreen.innerHTML = colors.join(" <br /> ");
+
+var colorGreen = colors.slice(1,3);
+prinToScreen.innerHTML = colorGreen.join(" <br /> ");
+
+var colorsCopy = colors.slice(colors);
+prinToScreen.innerHTML = colorsCopy.join(" <br /> ");
+
+prinToScreen.innerHTML = colors.indexOf("yellow");
+prinToScreen.innerHTML = colors.indexOf("blue");
+
+var cars = ["Audi A6", "Volvo 940", "Ford Mondeo", "Ferrari", "Landa", "Lada", "Citroën"];
+for (let index = 0; index < cars.length; index++) {
+    prinToScreen.innerHTML += cars[index] + "<br />";
+    
+}
+prinToScreen.innerHTML = cars.reverse();
+*/
+var prinToScreen = document.getElementById("testArray");
+
+
+var countries = ["Sweden", "Denmark", "Norway", "Finland", "Island"]
+countries.forEach(function(country){ 
+    prinToScreen.innerHTML += country + "<br />";
+})
