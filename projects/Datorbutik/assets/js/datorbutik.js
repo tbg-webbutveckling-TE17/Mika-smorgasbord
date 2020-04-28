ff0cce6e3dab979a244c6605cd9354193759974d
mikaparn <mikaparn@LSE-B3984>
2020-04-28 13:59:53 +0200

    har fortsatt med datorbutiken och har fixat med dropdown menues

--- projects/Datorbutik/assets/js/datorbutik.js
+++ projects/Datorbutik/assets/js/datorbutik.js
@@ -1,14 +1,4 @@
-function dropDownMenu() {
-    document.getElementById("dropdown").classList.toggle("show");
-}
-window.onclick = function(event) {
-        if (!event.target.matches(".dropbtn")) {
-        var dropdowns = document.getElementsByClassName("submenu");
-        for (var index = 0; index < dropdowns.length; index++) {
-            var openDropdown = dropdowns[index];
-            if (openDropdown.classList.contains("show")) {
-                openDropdown.classList.remove("show");}}}
-}
+
 
 var cart =
 
