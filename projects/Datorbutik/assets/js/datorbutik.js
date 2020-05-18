c3aa29a2e39b7bcd6df711a5f38ce29ce2bd1079
mikaparn <mikaparn@LSE-B3984>
2020-05-18 11:47:17 +0200

    continued working on datorbutiken and have started implementing more javascript

--- projects/Datorbutik/assets/js/datorbutik.js
+++ projects/Datorbutik/assets/js/datorbutik.js
@@ -2,14 +2,31 @@
 
 
 var cart = document.querySelector(".cartBtn");
-var itemsInCart = document.querySelector(".itemsInCart");
+var item = document.querySelector(".item");
 var itemBtn = document.querySelectorAll(".itemBtn");
-
-
+var itemCost = document.querySelector(".itemCost");
+var products = {
+    index0: {
+        name: "name",
+        cost: 50    
+    },
+    index2: {
+        name: "name",
+        cost: 50
+    },
+    index3: {
+        name: "name",
+        cost: 50
+    }
+};
 for (let index = 0; index < itemBtn.length; index++) {
     itemBtn[index].addEventListener("click", function() {
-        itemsInCart.innerHTML = 
-    })};
+        if (itemBtn[index] == 0) {
+          name = 10;
+        } 
+            item.innerHTML = products.name;
+            itemCost.innerHTML = products.cost + parseInt(itemCost.innerHTML) + "kr";
+        })};
 
 
 
