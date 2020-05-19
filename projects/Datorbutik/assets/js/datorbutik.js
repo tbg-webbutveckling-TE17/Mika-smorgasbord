e2800124e2892b2e473c5a1d2e61f16b259d95e8
mikaparn <mikaparn@LSE-B3984>
2020-05-19 13:58:35 +0200

    continued working on datorbutiken and made it so you can place items in the cart

--- projects/Datorbutik/assets/js/datorbutik.js
+++ projects/Datorbutik/assets/js/datorbutik.js
@@ -4,29 +4,37 @@
 var cart = document.querySelector(".cartBtn");
 var item = document.querySelector(".item");
 var itemBtn = document.querySelectorAll(".itemBtn");
-var itemCost = document.querySelector(".itemCost");
+var totalCost = document.querySelector(".totalCost");
 var products = {
     index0: {
         name: "name",
-        cost: 50    
+        cost: 125    
+    },
+    index1: {
+        name: "name",
+        cost: 250
     },
     index2: {
         name: "name",
-        cost: 50
-    },
-    index3: {
-        name: "name",
-        cost: 50
+        cost: 150
     }
 };
-for (let index = 0; index < itemBtn.length; index++) {
+for (var index = 0; index < itemBtn.length; index++) {
     itemBtn[index].addEventListener("click", function() {
-        if (itemBtn[index] == 0) {
-          name = 10;
-        } 
-            item.innerHTML = products.name;
-            itemCost.innerHTML = products.cost + parseInt(itemCost.innerHTML) + "kr";
-        })};
+        
+        if (this == itemBtn[0]) {
+            item.innerHTML = products.index0.name;
+            totalCost.innerHTML = products.index0.cost + parseInt(totalCost.innerHTML) + "kr";
+        }
+        else if (this == itemBtn[1]) {
+            item.innerHTML = products.index1.name;
+            totalCost.innerHTML = products.index1.cost + parseInt(totalCost.innerHTML) + "kr";
+          }
+        else {
+            item.innerHTML = products.index2.name;
+            totalCost.innerHTML = products.index2.cost + parseInt(totalCost.innerHTML) + "kr";
+        }
+    })};
 
 
 
