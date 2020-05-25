87a6f5d49c1ab90ef0c8871820f74e7ea6afed35
mikaparn <mikaparn@LSE-B3984>
2020-05-25 11:42:04 +0200

    continued with datorbutiken and added removebtn and purchasebtn functionality

--- projects/Datorbutik/assets/js/datorbutik.js
+++ projects/Datorbutik/assets/js/datorbutik.js
@@ -4,34 +4,45 @@
 var cart = document.querySelector(".cartBtn");
 var item = document.querySelector(".item");
 var itemBtn = document.querySelectorAll(".itemBtn");
+var purchaseBtn = document.querySelector(".purchaseBtn")
+var removeBtn = document.querySelector(".removeBtn")
 var totalCost = document.querySelector(".totalCost");
 var products = {
     index0: {
         name: "name",
-        cost: 125    
+        cost: 150   
     },
     index1: {
         name: "name",
-        cost: 250
+        cost: 50
     },
     index2: {
         name: "name",
-        cost: 150
+        cost: 100
     }
 };
+removeBtn.addEventListener("click", function() {
+    item.innerHTML = "";
+    totalCost.innerHTML = 0;
+    
+})
+purchaseBtn.addEventListener("click", function() {
+   item.innerHTML = "";
+   totalCost.innerHTML = 0;
+});
 for (var index = 0; index < itemBtn.length; index++) {
     itemBtn[index].addEventListener("click", function() {
         
         if (this == itemBtn[0]) {
-            item.innerHTML = products.index0.name;
+            item.innerHTML = products.index0.name + " + " + item.innerHTML;
             totalCost.innerHTML = products.index0.cost + parseInt(totalCost.innerHTML) + "kr";
         }
         else if (this == itemBtn[1]) {
-            item.innerHTML = products.index1.name;
+            item.innerHTML = products.index1.name + " + " + item.innerHTML;
             totalCost.innerHTML = products.index1.cost + parseInt(totalCost.innerHTML) + "kr";
           }
         else {
-            item.innerHTML = products.index2.name;
+            item.innerHTML = products.index2.name + " + " + item.innerHTML;
             totalCost.innerHTML = products.index2.cost + parseInt(totalCost.innerHTML) + "kr";
         }
     })};
